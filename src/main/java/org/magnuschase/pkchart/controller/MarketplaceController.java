33d316b894059a7d236f8bc363e5821c6d94387d
Jakub Kapała <kubusxzx@gmail.com>
2025-05-24 23:31:25 +0200

    Revert "feat: add services for newly created controllers"
    This reverts commit 34a50a8378ee0fbcbac7e298c622dcd6c7319389.

--- src/main/java/org/magnuschase/pkchart/controller/MarketplaceController.java
+++ src/main/java/org/magnuschase/pkchart/controller/MarketplaceController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
-@RequestMapping("/marketplace")
+@RequestMapping("/portfolio")
 @Tag(name = "Marketplace API", description = "Buy and sell cards in the marketplace")
 public class MarketplaceController {
 	
--- src/main/java/org/magnuschase/pkchart/service/PortfolioService.java
+++
@@ -1,8 +0,0 @@
-package org.magnuschase.pkchart.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class PortfolioService {
-	
-}
