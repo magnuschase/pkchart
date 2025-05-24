612ade41f02e0250a542f0f4247689466e9ab003
Jakub Kapała <kubusxzx@gmail.com>
2025-05-24 23:31:25 +0200

    Revert "feat: Portfolio API implementation"
    This reverts commit 0c7c963a35c2b9ab84a2f03b844c31eaa647d2c1.

--- src/main/java/org/magnuschase/pkchart/controller/PortfolioController.java
+++ src/main/java/org/magnuschase/pkchart/controller/PortfolioController.java
@@ -1,13 +1,7 @@
 package org.magnuschase.pkchart.controller;
 
-import org.magnuschase.pkchart.interfaces.PortfolioAddCardRequestDTO;
-import org.magnuschase.pkchart.interfaces.PortfolioDTO;
-import org.magnuschase.pkchart.service.PortfolioService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -15,35 +9,5 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @RequestMapping("/portfolio")
 @Tag(name = "Portfolio API", description = "Portfolio management")
 public class PortfolioController {
-    @Autowired
-    private PortfolioService portfolioService;
-
-    @GetMapping
-    public ResponseEntity<PortfolioDTO> getPortfolio(@AuthenticationPrincipal UserDetails userDetails) {
-        PortfolioDTO cardApprovalRequest =
-                portfolioService.getPortfolioForUser(userDetails);
-        return ResponseEntity.ok(cardApprovalRequest);
-    }
-
-    @PutMapping("/add/{cardId}")
-    public ResponseEntity<PortfolioDTO> addCard(
-            @AuthenticationPrincipal UserDetails userDetails,
-            @PathVariable Long cardId,
-            @RequestBody PortfolioAddCardRequestDTO portfolioAddCardRequestDTO
-            ) {
-
-        PortfolioDTO dto =
-                portfolioService.addCard(userDetails, portfolioAddCardRequestDTO, cardId);
-        return ResponseEntity.ok(dto);
-    }
-
-    @DeleteMapping("/remove/{entryId}")
-    public ResponseEntity<PortfolioDTO> removeCard(
-            @AuthenticationPrincipal UserDetails userDetails,
-            @PathVariable Long entryId
-    ) {
-        PortfolioDTO dto =
-                portfolioService.removeCard(userDetails, entryId);
-        return ResponseEntity.ok(dto);
-    }
+	
 }
