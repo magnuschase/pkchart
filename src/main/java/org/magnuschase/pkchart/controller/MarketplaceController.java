2ef4c84f3f033c3bcf5e53b74434677b08b4fbd7
Jakub Kapala <jakub.kapala@aptiv.com>
2025-05-24 17:55:26 +0200

    feat: add services for newly created controllers

--- src/main/java/org/magnuschase/pkchart/controller/MarketplaceController.java
+++ src/main/java/org/magnuschase/pkchart/controller/MarketplaceController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
-@RequestMapping("/portfolio")
+@RequestMapping("/marketplace")
 @Tag(name = "Marketplace API", description = "Buy and sell cards in the marketplace")
 public class MarketplaceController {
 	
